d723c20d6520c69d88ed48bf1bd30adc6e7249f5
windjammer1969 <wwebster@csumb.edu>
2016-06-16 23:55:23 -0700

    New Update from Wayne.

--- cst363-final/queryMusicStream_final.sql
+++ cst363-final/queryMusicStream_final.sql
@@ -11,7 +11,7 @@ SPOOL C:\Users\wwebster\Desktop\Mod3\JoeS\CSUMB\cst363-final\queryMusicStream_fi
 
 set linesize 255
 set pagesize 80
-select song.SONG_TITLE as "Song Title", album.ALBUM_TITLE as "Album Title", song.SONG_GENRE as "Album Genre" from song join album on song.ALBUM_ID = album.ALBUM_ID where song.SONG_GENRE = 'Rock';
+select song.SONG_TITLE as "Song Title", album.ALBUM_TITLE as "Album Title", ARTIST.ARTIST_NAME as "Artist", song.SONG_GENRE as "Album Genre" from song join album on song.ALBUM_ID = album.ALBUM_ID where song.SONG_GENRE = 'Rock';
 
 COLUMN ARTIST_INFO HEADING 'Artist |Information';
 COLUMN ARTIST_INFO FORMAT A20 WRAP;
