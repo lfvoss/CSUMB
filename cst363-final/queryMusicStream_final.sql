bd9e86e70e4bb8dec79ec5433770231c119a7d8d
windjammer1969 <wwebster@csumb.edu>
2016-06-16 23:47:06 -0700

    Updated query file with LVoss query

--- cst363-final/queryMusicStream_final.sql
+++ cst363-final/queryMusicStream_final.sql
@@ -1,5 +1,5 @@
 
-SPOOL C:\CST363\queryMusicStream_final.txt
+SPOOL C:\Users\wwebster\Desktop\Mod3\JoeS\CSUMB\cst363-final\queryMusicStream_final.txt
 
 /*
   Authors: Wayne Webster, Joe Sarabia, Lisa Voss, 
@@ -9,7 +9,13 @@ SPOOL C:\CST363\queryMusicStream_final.txt
   Instructor: Dr. Wendy Wang
 */
 
+set linesize 255
+set pagesize 80
+select song.SONG_TITLE as "Song Title", album.ALBUM_TITLE as "Album Title", song.SONG_GENRE as "Album Genre" from song join album on song.ALBUM_ID = album.ALBUM_ID where song.SONG_GENRE = 'Rock';
 
+COLUMN ARTIST_INFO HEADING 'Artist |Information';
+COLUMN ARTIST_INFO FORMAT A20 WRAP;
+SELECT * FROM ARTIST;
 
 SPOOL OFF
 
