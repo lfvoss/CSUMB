89ae360acab01162a15351ce593e7621c9eda488
Lisa <lfbenson@gmail.com>
2016-08-04 20:14:07 -0700

    CGP-2 Adding data to assets

--- cst336/z_assets.sql
+++ cst336/z_assets.sql
@@ -2,5 +2,18 @@ INSERT INTO `Z_Assets` (`name`) VALUES
 ('Ridley Scott'),
 ('Harrison Ford'),
 ('Rutger Hauer'),
-('Sean Young');
+('Sean Young'),
+('Daniel Day-Lewis'),
+('Cate Blanchett'),
+('Rod Steiger'),
+('Omar Shariff'),
+('David Lean'),
+('Julie Christie'),
+('Michael Mann'),
+('Woody Allen'),
+('Elia Kazan'),
+('Marlon Brando'),
+('Eva Marie-Saint'),
+('Alfred Hitchcock'),
+('Tippi Hedren');
 
