bf315548d231d4cb05b62a550abd54ebde2f2a18
Lisa <lfbenson@gmail.com>
2016-08-04 20:27:33 -0700

    Update z_movies.sql

--- cst336/z_movies.sql
+++ cst336/z_movies.sql
@@ -1,2 +1,17 @@
 INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
 ('Blade Runner', 1982, 'Do androids dream of electric sheep?', 'R', 116);
+
+INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
+('Doctor Zhivago', 1965, 'The life of a Russian physician and poet who, although married to another, falls in love with a political activist''s wife and experiences hardship during the First World War and then the October Revolution.', 'PG-13', 197);
+
+INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
+('The Last of the Mohicans', 1992, 'Three trappers protect of British Colonel''s daughters in the midst of the French and Indian War.', 'R', 112);
+
+INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
+('Blue Jasmine', 2013, 'A New York socialite, deeply troubled and in denial, arrives in San Francisco to impose upon her sister. She looks a million, but isn''t bringing money, peace, or love...', 'R', 98);
+
+INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
+('On the Waterfront', 1954, 'An ex-prize fighter turned longshoreman struggles to stand up to his corrupt union bosses.', 'Not Rated', 108);
+
+INSERT INTO `Z_Movies` (`title`, `year`, `description`, `rating`, `length`) VALUES
+('The Birds', 1963, 'A wealthy San Francisco socialite pursues a potential boyfriend to a small Northern California town that slowly takes a turn for the bizarre when birds of all kinds suddenly begin to attack people.', 'PG-13', 119);
