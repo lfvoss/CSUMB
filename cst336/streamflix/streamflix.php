219c76cd62c6adfd9b41cb5f7a9d43a3c75d8349
Joe Sarabia <jsarabia@csumb.edu>
2016-08-05 22:56:27 -0700

    CGP-17 initial version of streamflix main page, currently returns a summary of all movies

--- cst336/streamflix/streamflix.php
+++ cst336/streamflix/streamflix.php
@@ -1,3 +1,65 @@
 <?php
 	require '../db_connection.php';
+	
+	function getMovies(){
+	    global $dbConn;
+	    
+	    $sql = "SELECT title, year, rating, length
+	            FROM Z_Movies
+	            ORDER BY title";
+	    $stmt = $dbConn -> prepare($sql);
+	    $stmt -> execute();
+		
+	    return $stmt->fetchAll();
+	}
+	
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		
+		<title>StreamFlix Main Page</title>
+		<meta name="description" content="">
+		<meta name="author" content="lara4594">
+		
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	</head>
+	
+	<body>
+		<div>
+			
+			<table>
+				<tr>
+					<th>Movie</th>
+					<th>Year</th>
+					<th>Rating</th>
+					<th>Length</th>
+				</tr>
+				<?php
+					$movies = getMovies();
+
+					foreach($movies as $movie)
+					{
+						echo "<tr>";
+						echo "<td>" . $movie['title'] . "</td>";
+						echo "<td>" . $movie['year'] . "</td>";
+						echo "<td>" . $movie['rating'] . "</td>";
+						echo "<td>" . $movie['length'] . "</td>";
+						echo "</tr>";
+					}	
+				?>
+			</table>
+			
+		</div>
+	</body>
+</html>
