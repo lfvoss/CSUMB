fb8ba631195f270120aa76a6633fb1f2e4f56af1
Lisa <lfbenson@gmail.com>
2016-08-09 20:53:24 -0700

    added changepassword button link and stylesheet
    i couldn't find a link for user to get to change password page

--- cst336/streamflix/streamflix.php
+++ cst336/streamflix/streamflix.php
@@ -205,17 +205,24 @@
 		
 		<title>StreamFlix Main Page</title>
 		<meta name="description" content="">
-		<meta name="author" content="lara4594">
+		<meta name="author" content="Team Enhydra">
 		
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 		
 		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
 		<link rel="shortcut icon" href="/favicon.ico">
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<link rel="stylesheet" type="text/css" href="streamflix.css">
+
 	</head>
 	
 	<body>
 		<div>
+			<div>
+				<form method="post" action="streamflix_chpass.php">
+				<input type="submit" value="Change Password" />
+				</form>
+			</div>
 			<div>
 				<form method="post" action="streamflix_logout.php">
 					<input type="submit" value="Logout" />
